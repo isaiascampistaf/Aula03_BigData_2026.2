cab8ebf697e6d263f5d70e02dc842f9377ced143
isaiascampistaf <isaias.campistaf@gmail.com>
2026-08-10 20:43:32 -0300

    Implementa Algoritimo ex01

--- aula03/exemplo01.py
+++ aula03/exemplo01.py
@@ -1,2 +1,21 @@
 # GitHUB
 print("GitHub- Aula 03")
+
+# Um veiculo percorre 10km com cada litro de combustivel
+# Considere duas distancias percorridas e calcule a quantidade
+# de combustivel necessária para realizar o percurso total
+# Apresente a distancia total e o combustivel necessário.
+
+# Exemplo 01 - Veiculo 10km/1
+#Entrada
+CONSUMO= 10   #Quando em CAPS, é constante, valor fixo. 
+distancia1 = float(input("Informe a distancia: "))
+distancia2 = float(input("Informe a 2º distancia: "))
+
+#Processamento (algoritimo é composto por entrada, processamento, saida) pode ocorrer de nao ter processamento
+distancia_total= distancia1 + distancia2 
+combustivel= distancia_total / CONSUMO 
+
+#Saida
+print(f"Distancia percorrida: {distancia_total}")
+print(f"Consumo de {combustivel} litros")
